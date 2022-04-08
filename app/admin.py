e84e324fb620af0c9b2c41477eae80e117612b25
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-04-08 20:10:47 +0300

    register models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+admin.site.register(Project)
+admin.site.register(Profile)
+# admin.site.register(DesignRate)
+# admin.site.register(UsabilityRate)
+# admin.site.register(ContentRate)
