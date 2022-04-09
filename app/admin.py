f3777dda0692b2e290a6b09890cb7ee65c9142d5
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-04-09 12:56:20 +0300

    registered photo's model

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,10 @@
 from __future__ import unicode_literals
 from django.contrib import admin
 from .models import Profile, Projects
+from .models import photos
 
 # Register your models here.
 
 admin.site.register(Projects)
 admin.site.register(Profile)
+admin.site.register(photos)
