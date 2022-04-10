68198322765a7146a854abeac91fc58ecaf79a4f
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-04-11 00:54:27 +0300

    ensconce

--- app/models.py
+++ app/models.py
@@ -9,6 +9,7 @@ from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics/')
