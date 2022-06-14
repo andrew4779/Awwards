4049421251882d9f2a05578ca83a05f7987cb178
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-06-14 17:15:51 +0300

    deployment to heroku

--- awards/settings.py
+++ awards/settings.py
@@ -146,8 +146,8 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # # configuring the location for media
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # adding config
